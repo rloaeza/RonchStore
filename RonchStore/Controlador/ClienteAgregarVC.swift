2513588e6e40f45bbad25088bb8964d5d54bf594
Roberto Loaeza Valerio <roberto.loaeza@gmail.com>
2019-07-16 09:11:40 -0500

    Trabajando...

--- RonchStore/Controlador/ClienteAgregarVC.swift
+++ RonchStore/Controlador/ClienteAgregarVC.swift
@@ -10,50 +10,92 @@ import UIKit
 import FirebaseDatabase
 
 class ClienteAgregarVC: UIViewController {
+    
+    var cliente: NSDictionary? = nil
 
     @IBOutlet weak var telefono: UITextField!
     @IBOutlet weak var nombre: UITextField!
     @IBOutlet weak var direccion: UITextField!
     @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var botonEliminar: UIButton!
     
-    
-    @IBAction func botonAgregar(_ sender: Any) {
+    @IBAction func botonGuardar(_ sender: Any) {
+        if telefono.text!.isEmpty {
+            return
+        }
         var ref: DatabaseReference!
         ref = Database.database().reference()
-    
+        
         ref.child("Clientes").child(telefono.text!).setValue(["telefono": telefono.text!, "nombre": nombre.text!, "direccion": direccion.text!,"email": email.text!])
         
         
-        let alert = UIAlertController(title: "Clientes", message: "Cliente agregado.", preferredStyle: .actionSheet)
-        
-   
-        
+        let alert = UIAlertController(title: "Clientes", message: "Cliente guardado.", preferredStyle: .actionSheet)
         self.present(alert, animated: true)
         
         let when = DispatchTime.now() + 3
         DispatchQueue.main.asyncAfter(deadline: when){
             alert.dismiss(animated: true, completion: nil)
-            self.botonLimpiar(sender)
+            self.navigationController?.popViewController(animated: true)
 
         }
         
     }
     
-    @IBAction func botonLimpiar(_ sender: Any) {
+  
+    func limpiar() {
         telefono.text = ""
         nombre.text = ""
         direccion.text = ""
         email.text = ""
-        telefono.select(sender)
+        telefono.select(nil)
     }
     
-    @IBAction func botonCancelar(_ sender: Any) {
-        self.navigationController?.popViewController(animated: true)
+    @IBAction func botonEliminar(_ sender: Any) {
+        let alert = UIAlertController(title: "¿Eliminar?", message: "¿Eliminar a: \(telefono.text!)?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Si", style: .default, handler: { (UIAlertAction) in
+            var ref: DatabaseReference!
+            ref = Database.database().reference()
+            ref.child("Clientes").child(self.telefono.text!).setValue(nil)
+            self.navigationController?.popViewController(animated: true)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "No", style: .default, handler: { (UIAlertAction) in
+            alert.dismiss(animated: true, completion: nil)
+
+        }))
+        
+        self.present(alert, animated: true)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if cliente != nil {
+            telefono.text = cliente!.value(forKey: "telefono") as? String
+            nombre.text = cliente!.value(forKey: "nombre") as? String
+            direccion.text = cliente!.value(forKey: "direccion") as? String
+            email.text = cliente!.value(forKey: "email") as? String
+            telefono.isEnabled = false
+            botonEliminar.isHidden = false
+            cliente = nil
+        }
+        else {
+            limpiar()
+            telefono.isEnabled = true
+            botonEliminar.isHidden = true
+        }
     }
     
 
--- RonchStore/Controlador/ProductosVC.swift
+++ RonchStore/Controlador/ProductosVC.swift
@@ -1,45 +1,47 @@
 //
-//  ClientesVC.swift
+//  ProductosVC.swift
 //  RonchStore
 //
-//  Created by Roberto Loaeza Valerio on 7/13/19.
+//  Created by Roberto Loaeza Valerio on 7/16/19.
 //  Copyright © 2019 Roberto Loaeza Valerio. All rights reserved.
 //
 
 import UIKit
 import FirebaseDatabase
 
-class ClientesVC: UIViewController {
+class ProductosVC: UIViewController {
     var valores: [NSDictionary] = []
+
+    @IBOutlet weak var productosViewControler: UITableView!
     
-    @IBOutlet weak var tableViewClientes: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
         
         var ref: DatabaseReference!
-        ref = Database.database().reference().child("Clientes")
+        ref = Database.database().reference().child("Productos")
         
         ref.observe(.value) { (DataSnapshot) in
             self.valores.removeAll()
             for child in DataSnapshot.children {
                 if let snap = child as? DataSnapshot {
                     let dic = snap.value as? NSDictionary
+                    dic?.setValue(snap.key, forKey: "codigo")
                     self.valores.append(dic!)
                 }
             }
-            self.tableViewClientes.reloadData()
+            self.productosViewControler.reloadData()
         }
-
-        // Do any additional setup after loading the view.
     }
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "ClienteAgregarSegue",
-        let vc = segue.destination as? ClienteAgregarVC {
-            vc.cliente = sender as? NSDictionary
+        if segue.identifier == "ProductoAgregarSegue",
+            let vc = segue.destination as? ProductoAgregarVC {
+            vc.producto = sender as? NSDictionary
         }
     }
-    
 
     /*
     // MARK: - Navigation
@@ -52,24 +54,27 @@ class ClientesVC: UIViewController {
     */
 
 }
-extension ClientesVC:UITableViewDataSource {
+
+
+
+extension ProductosVC:UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return valores.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let celda = tableView.dequeueReusableCell(withIdentifier: "ClienteCelda", for: indexPath)
+        let celda = tableView.dequeueReusableCell(withIdentifier: "ProductoCelda", for: indexPath)
         celda.textLabel?.text = valores[indexPath.row].value(forKey: "nombre") as? String
-        celda.detailTextLabel?.text = valores[indexPath.row].value(forKey: "telefono") as? String
+        celda.detailTextLabel?.text = valores[indexPath.row].value(forKey: "marca") as? String
         return celda
     }
     
     
 }
-
-extension ClientesVC:UITableViewDelegate {
+extension ProductosVC:UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.performSegue(withIdentifier: "ClienteAgregarSegue", sender: valores[indexPath.row])
+        self.performSegue(withIdentifier: "ProductoAgregarSegue", sender: valores[indexPath.row])
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
+
