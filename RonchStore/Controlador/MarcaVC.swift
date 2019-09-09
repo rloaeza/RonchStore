f9e9230beba9fdab0db5082f16bca8711c9eeaf5
Roberto Loaeza Valerio <roberto.loaeza@gmail.com>
2019-09-09 14:38:21 -0500

    Marcas

---
+++ RonchStore/Controlador/MarcaVC.swift
@@ -0,0 +1,19 @@
+//
+//  MarcaVC.swift
+//  RonchStore
+//
+//  Created by Roberto Loaeza Valerio on 9/9/19.
+//  Copyright © 2019 Roberto Loaeza Valerio. All rights reserved.
+//
+
+import UIKit
+
+class MarcaVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
